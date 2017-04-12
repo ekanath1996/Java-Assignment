76fa6d8f8207530068b435db9ed98dfb717e43fc
ekanath1996 <ekan1996@gmail.com>
2017-04-12 15:01:54 +0530

    Assgmt 2

---
+++ ASGMT_ 2(java)/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee salary1=new Employee(1000,12);
+		System.out.println("\nMy salary is "+salary1.calculateSalary());
+		
+		Employee salary2=new Employee(2000,12);
+		System.out.println("\nMy salary is "+salary2.calculateSalary());
+		
+		Employee salary3=new Employee(3000,12);
+		System.out.println("\nMy salary is "+salary3.calculateSalary());
+		
+		
+		
+	}
+
+}
