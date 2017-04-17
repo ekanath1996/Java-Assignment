d406e23f203076d0e564d25912dc65c22d34defc
ekanath1996 <ekan1996@gmail.com>
2017-04-17 15:02:34 +0530

    create getter and setter

---
+++ ASIGMT _3ORG(java)/src/Employee.java
@@ -0,0 +1,39 @@
+
+public class Employee {
+
+	private int int_empno, int_salary;
+	private String str_name, str_dep;
+	public int getInt_empno() {
+		return int_empno;
+	}
+	public void setInt_empno(int int_empno) {
+		this.int_empno = int_empno;
+	}
+	public int getInt_salary() {
+		return int_salary;
+	}
+	public void setInt_salary(int int_salary) {
+		this.int_salary = int_salary;
+	}
+	public String getStr_name() {
+		return str_name;
+	}
+	public void setStr_name(String str_name) {
+		this.str_name = str_name;
+	}
+	public String getStr_dep() {
+		return str_dep;
+	}
+	public void setStr_dep(String str_dep) {
+		this.str_dep = str_dep;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
