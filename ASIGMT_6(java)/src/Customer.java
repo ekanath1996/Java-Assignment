48e3e7b124eb13c3667045e75a2651d58a5cc464
ekanath1996 <ekan1996@gmail.com>
2017-04-18 12:39:57 +0530

    constructor with three object

---
+++ ASIGMT_6(java)/src/Customer.java
@@ -0,0 +1,23 @@
+
+public class Customer {
+	
+	
+	int int_customer_id;
+	String str_mob;
+	
+	String str_customer_name, str_customer_city;
+	
+	void customer_details(int int_customer_id, String str_mob, String str_customer_name, String str_customer_city )
+	{
+		
+	 System.out.println("customer details\n.................. \n\n" +"customer id: "+int_customer_id +"\ncustomer name: "+str_customer_name +"\ncustomer city: "+str_customer_city +"\ncustomer Mob: "+str_mob+"\n\n");
+	 	
+		
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ ASIGMT_6(java)/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Customer ob1=new Customer();
+		Customer ob2=new Customer();
+		Customer ob3=new Customer();
+		
+		ob1.customer_details(22, "8086698101", "Ekanath", "calicut");
+		ob2.customer_details(23, "9586698102", "Raju", "Mangalore");
+		ob3.customer_details(24, "9786698103", "Dora", "Kochi");
+
+	}
+
+}
