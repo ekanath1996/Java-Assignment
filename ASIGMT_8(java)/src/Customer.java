8a8ad5554e600bffd49ecf3169a9442a2f239801
ekanath1996 <ekan1996@gmail.com>
2017-04-18 13:21:53 +0530

    customer class --> getter and setter --> 3_objects

---
+++ ASIGMT_8(java)/src/Customer.java
@@ -0,0 +1,55 @@
+
+public class Customer {
+
+	
+	int int_customer_id;
+	String str_mob;
+	
+	String str_customer_name, str_customer_city;
+
+	public int getInt_customer_id() {
+		return int_customer_id;
+	}
+
+	public void setInt_customer_id(int int_customer_id) {
+		this.int_customer_id = int_customer_id;
+	}
+
+	public String getStr_mob() {
+		return str_mob;
+	}
+
+	public void setStr_mob(String str_mob) {
+		this.str_mob = str_mob;
+	}
+
+	public String getStr_customer_name() {
+		return str_customer_name;
+	}
+
+	public void setStr_customer_name(String str_customer_name) {
+		this.str_customer_name = str_customer_name;
+	}
+
+	public String getStr_customer_city() {
+		return str_customer_city;
+	}
+
+	public void setStr_customer_city(String str_customer_city) {
+		this.str_customer_city = str_customer_city;
+	}
+	
+	void print()
+	{
+		
+		System.out.println("customer details\n.................. \n\n" +"customer id: "+int_customer_id +"\ncustomer name: "+str_customer_name +"\ncustomer city: "+str_customer_city +"\ncustomer Mob: "+str_mob+"\n\n");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ ASIGMT_8(java)/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Customer ob1=new Customer();
+		ob1.setInt_customer_id(22);
+		ob1.setStr_customer_name("Ekanath");
+        ob1.setStr_customer_city("Calicut");
+        ob1.setStr_mob("8086698101");
+		ob1.print();
+		
+        
+		Customer ob2=new Customer();
+		ob2.setInt_customer_id(23);
+		ob2.setStr_customer_name("Raju");
+        ob2.setStr_customer_city("Mangalore");
+        ob2.setStr_mob("9786698102");
+		ob2.print(); 
+        
+		Customer ob3=new Customer();
+		ob3.setInt_customer_id(22);
+		ob3.setStr_customer_name("Dora");
+        ob3.setStr_customer_city("Kochi");
+        ob3.setStr_mob("9586698103");
+		ob3.print(); 
+        
+        
+        
+        
+        
+		
+
+	}
+
+}
