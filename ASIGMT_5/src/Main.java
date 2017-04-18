b9522267176d8f1408720ef2ad44f7d5af80c0e4
ekanath1996 <ekan1996@gmail.com>
2017-04-18 12:13:18 +0530

    using constructor in customer class

---
+++ ASIGMT_5/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	   
+		Customer ob=new Customer();
+		
+		ob.customer_details(22, "8086698101", "Ekanath", "calicut");
+		
+		
+		
+
+	}
+
+}
